b706183e140031a2470a47fe71e5c7a90601bdac
XuyingSwift <xuyingswift@gmail.com>
2022-04-17 14:56:09 -0600

    updated the bash file

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 echo -n "Enter Node ID: "
 read nodeId
 
-configStr="0 127.0.0.1 6000 1 127.0.0.1 6001 2 127.0.0.1 6002"
+configStr="1 18.212.28.37 4000 2 52.207.209.55 4000 3 44.204.63.193 4000 4 18.118.51.255 4000 5 54.219.59.46 4000"
 args="$nodeId $configStr"
 echo $args
 
